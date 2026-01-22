574aee819384a94e7f7401fcc374a4ca9058461b
Igor Puris <igors.puris@gmail.com>
2026-01-23 00:40:09 +0100

    change log language to eng

--- src/instances/api.ts
+++ src/instances/api.ts
@@ -2,34 +2,34 @@ import axios from "axios";
 import i18next from "i18next";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use((config) => {
-  const lang = (i18next.language || "en").slice(0, 2);
-  if (config.headers) {
-    config.headers['Accept-Language'] = lang;
-  }
-  return config;
+    const lang = (i18next.language || "en").slice(0, 2);
+    if (config.headers) {
+        config.headers["Accept-Language"] = lang;
+    }
+    return config;
 });
 
 const reloadCallbacks: Array<() => void> = [];
 
 i18next.on("languageChanged", () => {
-  console.log("Язык сменился, повторяем запросы...");
-  reloadCallbacks.forEach((cb) => cb());
+    console.log("Language changed, triggering reload callbacks...");
+    reloadCallbacks.forEach((cb) => cb());
 });
 
 export const registerReloadOnLanguageChange = (callback: () => void) => {
-  reloadCallbacks.push(callback);
+    reloadCallbacks.push(callback);
 
-  return () => {
-    const index = reloadCallbacks.indexOf(callback);
-    if (index > -1) reloadCallbacks.splice(index, 1);
-  };
+    return () => {
+        const index = reloadCallbacks.indexOf(callback);
+        if (index > -1) reloadCallbacks.splice(index, 1);
+    };
 };
 
 export default api;
